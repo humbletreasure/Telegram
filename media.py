49e72a69d99780eba5bee276ea184a88aa879cb0
Mathias Andrew <mathiasnkereuwemandrew@gmail.com>
2025-12-19 15:46:21 +0100

    Create media.py

---
+++ media.py
@@ -0,0 +1,5 @@
+# media.py
+def upload_video(*args, **kwargs): pass
+def upload_picture(*args, **kwargs): pass
+def get_next_video_for_user(*args, **kwargs): return None
+def get_next_picture_for_user(*args, **kwargs): return None
